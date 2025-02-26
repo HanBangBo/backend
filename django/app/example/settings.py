b76a1ff68979cc2ed37200b10ea348f2712cf6c5
김동규 <dongu@gimdong-gyuui-MacBookPro.local>
2025-02-26 22:31:43 +0900

    Test External Access

--- django/app/example/settings.py
+++ django/app/example/settings.py
@@ -34,7 +34,8 @@ SECRET_KEY = env('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ("localhost", "127.0.0.1"))
+# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ("localhost", "127.0.0.1"))
+ALLOWED_HOSTS = ["*"]  # [test] 모든 외부 접속 허용 (보안 취약)
 
 
 # Application definition
